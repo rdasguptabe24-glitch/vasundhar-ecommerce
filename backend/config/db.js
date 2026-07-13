df706f0a0519475092ae40b382b439d22bd72a65
Raktim Dasgupta <rdasgupta_be24@thapar.edu>
2026-07-13 22:07:37 +0530

    Fix Database Issue

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
 
-        await mongoose.connect("mongodb://127.0.0.1:27017/vasundhara");
-
-        console.log("✅ MongoDB Connected");
-
-    } catch (error) {
-
-        console.error("❌ Database Connection Failed");
-        console.error(error.message);
-
-        process.exit(1);
-
-    }
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
