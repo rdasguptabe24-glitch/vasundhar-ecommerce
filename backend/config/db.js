ee5f95573d6f471a8d510617528fa5697946f512
Raktim Dasgupta <rdasgupta_be24@thapar.edu>
2026-07-14 11:08:34 +0530

    Debug database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,6 +5,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB Connected");
+    console.log("Database:", mongoose.connection.name);
   } catch (err) {
     console.error(err);
     process.exit(1);
